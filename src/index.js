498a64f4d1f1e02fe6ede170a82e9cafcaf8292b
Zijing Huang <yugihuang@gmail.com>
2020-06-23 14:57:23 +0800

    asset management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './410581.png'
+import Data from './data.xml'
 
 function component() {
   var element = document.createElement('div')
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+  element.classList.add('hello')
+
+  var myIcon = new Image()
+  myIcon.src = Icon
+
+  element.appendChild(myIcon)
+
+  console.log(Data)
 
   return element
 }
