85a401e25db51ba980c3faeaf894e1ce380f565c
Zijing Huang <yugihuang@gmail.com>
2020-06-23 16:14:46 +0800

    server

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,12 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       title: 'Output Management',
     }),
@@ -16,6 +20,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   module: {
     rules: [
