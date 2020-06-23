d1c700b9d3c6063668555933671c022d465d09bd
Zijing Huang <yugihuang@gmail.com>
2020-06-23 16:28:09 +0800

    HMR

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash'
 import printMe from './print.js'
+import './style.css'
 
 function component() {
   var element = document.createElement('div')
@@ -11,8 +12,18 @@ function component() {
   btn.onclick = printMe
 
   element.appendChild(btn)
-  
+
   return element
 }
 
-document.body.appendChild(component())
+let element = component()
+document.body.appendChild(element)
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!')
+    document.body.removeChild(element)
+    element = component()
+    document.body.appendChild(element)
+  })
+}
--- src/print.js
+++ src/print.js
@@ -1,3 +1,4 @@
 export default function printMe() {
-  console.log('I get called from print.js!2')
+  // console.log('I get called from print.js!2')
+  console.log('Updating print.js...')
 }
